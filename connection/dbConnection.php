b3086638f0c382a051a4926702f9e4b25cd44aa1
Cosmin N <57816741+cosmin-web@users.noreply.github.com>
2020-05-19 15:50:53 +0100

    Update dbConnection.php

--- connection/dbConnection.php
+++ connection/dbConnection.php
@@ -2,7 +2,7 @@
 //setting the date-time to britain global time
 date_default_timezone_set("GMT");
 //establishing the connection with the database
-$connection = mysqli_connect("localhost", "root", "kodita1991", "ltdb");
+$connection = mysqli_connect("localhost", "root", "", "ltdb");
 //if connection is not being made throw an error message
 if (!$connection) {
 	die("Connection Failed: " .mysqli_connect_error());
